e7964acc8b508a06e0b5825eba5ac600e55ef127
LBDonovan <liam.donovan@hotmail.co.uk>
2017-08-21 21:43:01 +0100

    register major and minor numbers for char device

--- hello.c
+++ hello.c
@@ -1,13 +1,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+int major= 0;
+int minor = 0;
+
 static int hello_init(void){
+	dev_t dev = 0;
+	int res;
+
 	printk(KERN_ALERT "Hello, world\n");
+
+	res = alloc_chrdev_region(&dev, 0, 1, "hello");
+	if (res < 0){
+		printk(KERN_WARNING "can't allocate major number\n");
+		return res;
+	}
+	major = MAJOR(dev);
+	printk(KERN_ALERT "Major number: %d\n", major);
 	return 0; 
 }
 
 static void hello_exit(void){
+	dev_t devno = MKDEV(major, minor);
+	unregister_chrdev_region(devno, 1);
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
