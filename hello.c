c04c594ba1af0683b89d6dea5815689db2fd23a2
LBDonovan <liam.donovan@hotmail.co.uk>
2017-08-22 09:36:08 +0100

    working echo character device

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
+#include <asm/uaccess.h>
 #include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/cdev.h>
@@ -12,20 +13,81 @@ int minor = 0;
 // main device structure
 struct hello_dev {
 	struct cdev cdev;
+	void *buf;
+	size_t size;
+	bool allocated;
 };
 
 // global pointer to main device structure, inited in hello_init
 struct hello_dev *hello_device;
 
+// function called when character device is opened
 static int hello_open(struct inode *inode, struct file *filp){
+	// inode is a struct representing the kernels internal view of the device file
+	// filp is a struct representing the currently open(ing) file descriptor
+	
+	// inode contains a pointer i_cdev to the character device struct initialised in hello_init
+	// here we use container_of to get a reference to the global device struct from the i_cdev pointer
+	// in this case, this is trivial and the same as: dev = hello_device; but is neccessary if there are multiple devices
+	struct hello_dev *dev;
+	dev = container_of(inode->i_cdev, struct hello_dev, cdev);
+	
+	// the reference to the global struct is then stored in the file descriptor struct for easy access later
+	filp->private_data = dev;
+
 	printk(KERN_ALERT "hello_open!\n");
 	return 0;
 }
 
+// function called when the device is closed
+static int hello_release(struct inode *inode, struct file *filp){
+
+	printk(KERN_ALERT "hello_release!\n");
+	return 0;
+}
+
+ssize_t hello_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos){
+	// retrieve the global dev struct from the function descriptor
+	struct hello_dev *dev;
+	dev = filp->private_data;
+
+	// save the user buffer into the device's persistant buffer
+	printk(KERN_ALERT "saving %d bytes\n", count);
+	copy_from_user(dev->buf, buf, count);
+	dev->size = count;
+
+	return count;
+}
+
+ssize_t hello_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
+	size_t read_count;
+	// retrieve the global dev struct from the function descriptor
+	struct hello_dev *dev;
+	dev = filp->private_data;
+
+	printk(KERN_ALERT "attempting to read %i bytes, size: %i\n", count, dev->size);
+
+	if (count > dev->size){
+		printk(KERN_ALERT "truncating read to %i bytes\n", dev->size);
+		read_count = dev->size;
+	} else {
+		read_count = count;
+	}
+
+	// copy the contents of the persistant buffer into the user buffer
+	copy_to_user(buf, dev->buf, read_count);
+	dev->size -= read_count;
+
+	return read_count;
+}
+
 // function pointer struct
 struct file_operations hello_fops = {
-	.owner = THIS_MODULE,
-	.open = hello_open,
+	.owner		= THIS_MODULE,
+	.open		= hello_open,
+	.release	= hello_release,
+	.read		= hello_read,
+	.write		= hello_write,
 };
 
 static int hello_init(void){
@@ -48,6 +110,10 @@ static int hello_init(void){
 	hello_device = kmalloc(sizeof(struct hello_dev), GFP_KERNEL);
 	memset(hello_device, 0, sizeof(struct hello_dev));
 
+	// and the main persistant buffer
+	hello_device->buf = kmalloc(4096, GFP_KERNEL);
+	memset(hello_device->buf, 0, 4096);
+
 	// initialise the underlying character device, storing its address in the global dev struct
 	cdev_init(&hello_device->cdev, &hello_fops);
 	hello_device->cdev.owner = THIS_MODULE;
@@ -61,6 +127,8 @@ static int hello_init(void){
 static void hello_exit(void){
 	dev_t devno = MKDEV(major, minor);
 	cdev_del(&hello_device->cdev);
+	kfree(hello_device->buf);
+	kfree(hello_device);
 	unregister_chrdev_region(devno, 1);
 	printk(KERN_ALERT "Goodbye world\n");
 }
