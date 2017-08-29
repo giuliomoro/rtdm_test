fef485f396015aee1c96413c38e8a58554a1d542
LBDonovan <liam.donovan@hotmail.co.uk>
2017-08-29 13:38:08 +0100

    add GPIO interrupt support (including irq_domain mapping) to hello.ko

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <asm/uaccess.h>
+#include <linux/interrupt.h>
 #include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/cdev.h>
@@ -15,7 +16,8 @@ struct hello_dev {
 	struct cdev cdev;
 	void *buf;
 	size_t size;
-	bool allocated;
+	void* gpio1_addr;
+	int irq_n;
 };
 
 // global pointer to main device structure, inited in hello_init
@@ -81,6 +83,18 @@ ssize_t hello_read(struct file *filp, char __user *buf, size_t count, loff_t *f_
 	return read_count;
 }
 
+irqreturn_t gpio1_irq_handler(int irq, void* dev_id){
+	unsigned int irq_status;
+	struct hello_dev *dev = (struct hello_dev*)dev_id;
+
+	// clear interrupt bit in gpio registers to prevent irq refiring
+	irq_status = ioread32(dev->gpio1_addr + 0x2c);
+	iowrite32(irq_status, dev->gpio1_addr + 0x2c);
+	printk(KERN_ALERT "irq %i: %08x\n", irq, irq_status);
+
+	return IRQ_HANDLED;
+}
+
 // function pointer struct
 struct file_operations hello_fops = {
 	.owner		= THIS_MODULE,
@@ -94,6 +108,16 @@ static int hello_init(void){
 	// init a device number variable
 	dev_t devno = 0;
 	int res;
+	void* cm_per_addr;
+	void* intc_addr;
+	unsigned int gpio1_clk;
+
+	// get reference to interrupt controller device from device tree
+	// there may be better ways to do this
+	// note, the device must be disabled in the device tree for this to work
+	struct device_node *of_node = of_find_node_by_name(NULL, "interrupt-controller");
+	// get intc's irq_domain - needed to map hwirq to irqs linux recieves
+	struct irq_domain *intc_domain = irq_find_matching_fwnode(&of_node->fwnode, DOMAIN_BUS_ANY); 
 
 	printk(KERN_ALERT "Hello world\n");
 
@@ -114,6 +138,28 @@ static int hello_init(void){
 	hello_device->buf = kmalloc(4096, GFP_KERNEL);
 	memset(hello_device->buf, 0, 4096);
 
+	// turn on the gpio1 interface clock
+	cm_per_addr = ioremap(0x44e00000, 0x3FFF);
+	gpio1_clk = ioread32(cm_per_addr + 0xac);
+	printk(KERN_ALERT "gpio1_clk: %08x\n", gpio1_clk);
+	if (gpio1_clk != 0x2){
+		iowrite32(0x2, cm_per_addr + 0xac);
+		printk(KERN_ALERT "turned on gpio1 ocp clock\n");
+	}
+	iounmap(cm_per_addr);
+
+	// map hwirq 98 (gpio1 interrupt from trm) to a software irq and store it in device struct
+	hello_device->irq_n = irq_create_mapping(intc_domain, 98);
+	res = request_irq(hello_device->irq_n, gpio1_irq_handler, IRQF_SHARED, "hello_irq", (void*)hello_device);
+	if (res != 0)
+		printk(KERN_ALERT "irq not registered: %i\n", res);
+
+	// activate falling-edge interrupts on gpio1_28
+	hello_device->gpio1_addr = ioremap(0x4804c000, 0xfff);
+	printk(KERN_ALERT "gpio1 rev: %08x\n", ioread32(hello_device->gpio1_addr));
+	iowrite32(0x10000000, hello_device->gpio1_addr+0x34);
+	iowrite32(0x10000000, hello_device->gpio1_addr+0x14c);
+
 	// initialise the underlying character device, storing its address in the global dev struct
 	cdev_init(&hello_device->cdev, &hello_fops);
 	hello_device->cdev.owner = THIS_MODULE;
@@ -127,6 +173,8 @@ static int hello_init(void){
 static void hello_exit(void){
 	dev_t devno = MKDEV(major, minor);
 	cdev_del(&hello_device->cdev);
+	free_irq(hello_device->irq_n, hello_device);
+	iounmap(hello_device->gpio1_addr);
 	kfree(hello_device->buf);
 	kfree(hello_device);
 	unregister_chrdev_region(devno, 1);
